c1cad3abbfdd83975f5e5768fa85fb0d253b3af1
Matthew Franz C. Oco <mcoco@up.edu.ph>
2021-04-07 04:16:47 +0800

    Update notebook

--- .virtual_documents/sports-eda.ipynb.py
+++ .virtual_documents/sports-eda.ipynb.py
@@ -4,23 +4,23 @@ import matplotlib.pyplot as plt
 get_ipython().run_line_magic("matplotlib", " inline")
 
 
-matchesDf = pd.read_csv('data/matches.csv')
-deliveriesDf = pd.read_csv('data/deliveries.csv')
+matches = pd.read_csv('data/matches.csv')
+deliveries = pd.read_csv('data/deliveries.csv')
 
 
-deliveriesDf.head()
+deliveries.head()
 
 
-matchesDf.head()
+matches.head()
 
 
-deliveriesDf.shape
+deliveries.shape
 
 
-matchesDf.shape
+matches.shape
 
 
-data = matchesDf.merge(deliveriesDf, left_on='id', right_on='match_id')
+data = matches.merge(deliveries, left_on='id', right_on='match_id')
 
 
 data.head()
@@ -36,7 +36,7 @@ data.isnull().sum().sort_values(ascending=False)
 
 
 # count the win by each team
-sucTeam = matchesDf['winner'].value_counts().to_frame()
+sucTeam = matches['winner'].value_counts().to_frame()
 
 # rename column
 sucTeam.rename(columns={'winner':'Count of Wins'}, inplace=True)
@@ -47,7 +47,7 @@ print('The most successful team is {}.'.format(sucTeam.index[0]))
 
 
 # count how many times the player become the player of the match
-playerOfMatch = matchesDf['player_of_match'].value_counts().to_frame()
+playerOfMatch = matches['player_of_match'].value_counts().to_frame()
 
 # rename column
 playerOfMatch.rename(columns={'player_of_match':'Player of the Match Count'}, inplace=True)
@@ -55,6 +55,11 @@ playerOfMatch.head()
 
 
 print('The most successful player is {}'.format(playerOfMatch.index[0]))
+
+
+batsmanGrp = deliveries.groupby(["match_id", "inning", "batting_team", "batsman"])
+batsmen = batsmanGrp["batsman_runs"].sum().reset_index()
+batsmen
 
 
 
